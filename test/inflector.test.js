3dd2941f50207a4c8e936af8cb4f5c2cbe3c5fa2
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2013-12-24 16:14:22 +0100

    Added inflector test

---
+++ test/inflector.test.js
@@ -0,0 +1,66 @@
+var should = require('should');
+var inflector = require('../lib/inflector/inflector.js');
+
+function isSingular(word, expected) {
+    it(word + (expected ? ' should be singular' : ' should not be singular'), function(done) {
+        inflector.isSingular(word).should.equal(expected);
+        done();
+    });
+}
+
+function pluralize(word, expected) {
+    it(word + ' should return the string ' + expected, function(done) {
+        inflector.pluralize(word).should.equal(expected);
+        done();
+    });
+}
+
+function isPlural (word, expected) {
+    it(word + (expected ? ' should be plural' : ' should not be plural'), function(done) {
+        inflector.isPlural(word).should.equal(expected);
+        done();
+    });
+}
+
+function singularize(word, expected) {
+    it(word + ' should return the string ' + expected, function(done) {
+        inflector.singularize(word).should.equal(expected);
+        done();
+    });
+}
+
+
+describe('Inflector', function() {
+
+    describe('pluralize', function() {
+        pluralize('wolf', 'wolves');
+        pluralize('alumunium', 'alumunia');
+        pluralize('formula', 'formulae');
+    });
+
+    describe('isPlural', function() {
+        isPlural('wolves', true);
+        isPlural('alumunia', true);
+        isPlural('formulae', true);
+
+        isPlural('wolf', false);
+        isPlural('alumunium', false);
+        isPlural('formula', false);
+    });
+
+    describe('singularize', function() {
+        singularize('wolves', 'wolf');
+        singularize('alumunia', 'alumunium');
+        singularize('formulae', 'formula');
+    });
+
+    describe('isSingular', function() {
+        isSingular('wolf', true);
+        isSingular('alumunium', true);
+        isSingular('formula', true);
+
+        isSingular('wolves', false);
+        isSingular('alumunia', false);
+        isSingular('formulae', false);
+    });
+});
